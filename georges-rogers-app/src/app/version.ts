5093097ad6e60219f08be3b8471750b675d4cc77
robert <robert@weightechinc.com>
2025-08-22 12:20:13 -0500

    build5

--- georges-rogers-app/src/app/version.ts
+++ georges-rogers-app/src/app/version.ts
@@ -1,5 +1,5 @@
 // Auto-generated file
 export const BUILD_NAME = 'georges-rogers-app';
-export const BUILD_VERSION = '4';
-export const BUILD_DATE = '2025-08-21T18:27:41.339Z';
-export const BUILD_INFO = '@(#)georges-rogers-app build 4';
+export const BUILD_VERSION = '5';
+export const BUILD_DATE = '2025-08-22T17:08:22.542Z';
+export const BUILD_INFO = '@(#)georges-rogers-app build 5';
