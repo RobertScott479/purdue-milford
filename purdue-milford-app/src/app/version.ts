99293ae68da763166328a63a232741867a2f12b5
robert <robert@weightechinc.com>
2025-11-25 12:32:41 -0600

    build11

--- purdue-milford-app/src/app/version.ts
+++ purdue-milford-app/src/app/version.ts
@@ -1,5 +1,5 @@
 // Auto-generated file
 export const BUILD_NAME = 'purdue-milford-app';
-export const BUILD_VERSION = '10';
-export const BUILD_DATE = '2025-11-17T16:49:15.132Z';
-export const BUILD_INFO = '@(#)purdue-milford-app build 10';
+export const BUILD_VERSION = '11';
+export const BUILD_DATE = '2025-11-25T18:31:06.202Z';
+export const BUILD_INFO = '@(#)purdue-milford-app build 11';
